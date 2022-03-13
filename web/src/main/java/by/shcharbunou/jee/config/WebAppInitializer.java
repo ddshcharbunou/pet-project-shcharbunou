ef4e89eb2227d382f76ba7d29916704def28fde2
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-13 16:29:51 +0300

    feat: add WebAppInitializer

---
+++ web/src/main/java/by/shcharbunou/jee/config/WebAppInitializer.java
@@ -0,0 +1,23 @@
+package by.shcharbunou.jee.config;
+
+import by.shcharbunou.dal.config.DalConfig;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{DalConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    @NonNull
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
