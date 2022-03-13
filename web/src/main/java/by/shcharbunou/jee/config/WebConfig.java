c123ca85d0f3a0cf9df95ca62f234baa5595c53d
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-13 16:22:39 +0300

    feat: add log4j

--- web/src/main/java/by/shcharbunou/jee/config/WebConfig.java
+++ web/src/main/java/by/shcharbunou/jee/config/WebConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("by.shcharbunou.jee")
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
     @Bean
     ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
