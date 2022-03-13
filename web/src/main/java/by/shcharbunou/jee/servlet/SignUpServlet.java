af819b444a7d4e9d796be49d0600d4d217d44dfd
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-13 17:00:09 +0300

    fix: refactor pom and add javax

--- web/src/main/java/by/shcharbunou/jee/servlet/SignUpServlet.java
+++ web/src/main/java/by/shcharbunou/jee/servlet/SignUpServlet.java
@@ -1,18 +1,25 @@
 package by.shcharbunou.jee.servlet;
 
 import by.shcharbunou.core.service.user.UserService;
-import by.shcharbunou.dal.dao.user.impl.UserDaoImpl;
 import by.shcharbunou.dal.entity.user.User;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 
 import java.io.IOException;
 
 @WebServlet(name = "SignUpServlet", value = "/pages/sign-up")
 public class SignUpServlet extends HttpServlet {
+    private ApplicationContext context;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        context = (ApplicationContext) getServletContext().getAttribute("context");
+    }
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
@@ -21,7 +28,7 @@ public class SignUpServlet extends HttpServlet {
         String email = req.getParameter("email");
         String phone = req.getParameter("phone_number");
         String password = req.getParameter("password");
-        UserService userService = new UserService(new UserDaoImpl());
+        UserService userService = context.getBean("userService", UserService.class);
         User user = new User();
         user.setName(name);
         user.setSurname(surname);
