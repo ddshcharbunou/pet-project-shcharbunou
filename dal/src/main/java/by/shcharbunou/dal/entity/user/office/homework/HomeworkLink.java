47cd382448f391e8f7924ab75e9e8535e2edccc9
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-17 21:06:48 +0300

    feat: all homework classes are connected now

--- dal/src/main/java/by/shcharbunou/dal/entity/user/office/homework/HomeworkLink.java
+++ dal/src/main/java/by/shcharbunou/dal/entity/user/office/homework/HomeworkLink.java
@@ -1,5 +1,6 @@
 package by.shcharbunou.dal.entity.user.office.homework;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,7 +8,11 @@ import javax.persistence.Embeddable;
 
 @Getter
 @Setter
+@AllArgsConstructor
+
 @Embeddable
 public class HomeworkLink {
+    private String taskType;
+
     private String link;
 }
--- dal/src/main/java/by/shcharbunou/dal/entity/user/office/homework/SubTheme.java
+++ dal/src/main/java/by/shcharbunou/dal/entity/user/office/homework/SubTheme.java
@@ -3,21 +3,22 @@ package by.shcharbunou.dal.entity.user.office.homework;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
-import javax.persistence.Embeddable;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+
 @Embeddable
 public class SubTheme {
     private String designation;
 
     @ElementCollection
-    @AttributeOverride(name = "link", column = @Column(name = "homework_link"))
+    @AttributeOverrides({
+            @AttributeOverride(name = "link", column = @Column(name = "homework_link")),
+            @AttributeOverride(name = "taskType", column = @Column(name = "homework_task_type"))
+    })
     private List<HomeworkLink> links = new ArrayList<>();
 
     public void connectLink(HomeworkLink link) {
