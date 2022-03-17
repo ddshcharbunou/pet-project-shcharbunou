dd293d62f7f47217fefe8d448e57164eeba6127c
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-17 11:31:38 +0300

    feat: add image and gallery entities

--- dal/src/main/java/by/shcharbunou/dal/entity/blog/gallery/Gallery.java
+++ dal/src/main/java/by/shcharbunou/dal/entity/blog/gallery/Gallery.java
@@ -1,4 +1,28 @@
 package by.shcharbunou.dal.entity.blog.gallery;
 
-public class Gallery {
+import by.shcharbunou.dal.entity.BaseEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+
+@Entity
+@Table(name = "gallery")
+@AttributeOverride(name = "id", column = @Column(name = "gallery_id"))
+public class Gallery extends BaseEntity {
+    @Column(name = "year", nullable = false)
+    private int year;
+
+    @OneToMany(mappedBy = "gallery", cascade = CascadeType.ALL)
+    private Set<Image> images = new HashSet<>();
+
+    @Deprecated
+    public Gallery() {}
 }
--- dal/src/main/java/by/shcharbunou/dal/entity/blog/gallery/Image.java
+++ dal/src/main/java/by/shcharbunou/dal/entity/blog/gallery/Image.java
@@ -1,4 +1,28 @@
 package by.shcharbunou.dal.entity.blog.gallery;
 
-public class Image {
+import by.shcharbunou.dal.entity.BaseEntity;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(callSuper = false)
+@AllArgsConstructor
+
+@Entity
+@Table(name = "image")
+@AttributeOverride(name = "id", column = @Column(name = "image_id"))
+public class Image extends BaseEntity {
+    @Column(name = "designation", nullable = false)
+    private String designation;
+
+    @ManyToOne
+    @JoinColumn(name = "gallery_id")
+    private Gallery gallery;
+
+    @Deprecated
+    public Image() {}
 }
