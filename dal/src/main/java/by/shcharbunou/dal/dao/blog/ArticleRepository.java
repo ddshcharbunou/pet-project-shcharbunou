0663222a729996ad8907cd632e7c265bf0e35fff
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-17 11:41:01 +0300

    feat: ArticleRepo is bean now

--- dal/src/main/java/by/shcharbunou/dal/dao/blog/ArticleRepository.java
+++ dal/src/main/java/by/shcharbunou/dal/dao/blog/ArticleRepository.java
@@ -2,9 +2,11 @@ package by.shcharbunou.dal.dao.blog;
 
 import by.shcharbunou.dal.entity.blog.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository("articleRepository")
 public interface ArticleRepository extends JpaRepository<Article, UUID> {
 
 }
