f79021d9e35c2a6b2082ca5fa8df7c99ef4f7c38
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-18 13:13:36 +0300

    feat: add functionality to UserService

--- core/src/main/java/by/shcharbunou/core/service/user/UserService.java
+++ core/src/main/java/by/shcharbunou/core/service/user/UserService.java
@@ -1,4 +1,45 @@
 package by.shcharbunou.core.service.user;
 
+import by.shcharbunou.dal.entity.user.User;
+
+import java.util.UUID;
+
+/**
+ * Service for User entity.
+ */
 public interface UserService {
+    /**
+     * Save user.
+     * @param user {@link User}
+     * @return user {@link User}
+     */
+    User saveUser(User user);
+
+    /**
+     * Find user by id.
+     * @param id id
+     * @return user {@link User}
+     */
+    User findUserById(UUID id);
+
+    /**
+     * Delete user;
+     * @param user {@link User}
+     */
+    void deleteUser(User user);
+
+    /**
+     * Find user by username.
+     * @param username username
+     * @return user {@link User}
+     */
+    User findUserByUsername(String username);
+
+    /**
+     * Find user by username and email.
+     * @param username username
+     * @param email email
+     * @return user {@link User}
+     */
+    User findUserByUsernameAndEmail(String username, String email);
 }
