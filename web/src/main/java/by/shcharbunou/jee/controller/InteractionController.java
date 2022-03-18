ffe1e4f8932ed7ac627a6958e65f30ad2ba19625
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-18 15:13:25 +0300

    feat: add InteractionController

---
+++ web/src/main/java/by/shcharbunou/jee/controller/InteractionController.java
@@ -0,0 +1,9 @@
+package by.shcharbunou.jee.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+
+@Log4j2
+@Controller
+public class InteractionController {
+}
