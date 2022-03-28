c2461a54a940bb79bef8f3402d51247cc0443556
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-03-28 15:05:51 +0300

    fix: fixed some troubles in security config

--- web/src/main/java/by/shcharbunou/jee/config/SecurityConfig.java
+++ web/src/main/java/by/shcharbunou/jee/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
