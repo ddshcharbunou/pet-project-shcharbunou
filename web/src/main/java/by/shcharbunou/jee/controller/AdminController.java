3947f982cbf1ac9295e8b218a2abb5b72bdba8be
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-04-01 12:07:41 +0300

    feat: add session attributes to controllers

--- web/src/main/java/by/shcharbunou/jee/controller/AdminController.java
+++ web/src/main/java/by/shcharbunou/jee/controller/AdminController.java
@@ -2,8 +2,10 @@ package by.shcharbunou.jee.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Log4j2
 @Controller
+@SessionAttributes({"user", "ROLE"})
 public class AdminController {
 }
--- web/src/main/java/by/shcharbunou/jee/controller/SurfaceController.java
+++ web/src/main/java/by/shcharbunou/jee/controller/SurfaceController.java
@@ -4,10 +4,12 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @Log4j2
 @Controller
+@SessionAttributes({"user", "ROLE"})
 public class SurfaceController {
     @GetMapping("/")
     public ModelAndView getIndexPage() {
