77a33b26bd18df1458f0c3082573b4dda1901c8a
Daniel Shcharbunou <danielshcharbunou@gmail.com>
2022-04-01 22:10:57 +0300

    feat: add java doc to article service

--- core/src/main/java/by/shcharbunou/core/service/blog/ArticleService.java
+++ core/src/main/java/by/shcharbunou/core/service/blog/ArticleService.java
@@ -6,14 +6,43 @@ import by.shcharbunou.dal.entity.blog.Article;
 
 import java.util.UUID;
 
+/**
+ * Service for Article entity.
+ */
 public interface ArticleService {
+    /**
+     * Save article in db.
+     * @param article article
+     * @return article {@link Article}
+     */
     Article saveArticle(Article article);
 
+    /**
+     * Delete article
+     * @param article article
+     */
     void deleteArticle(Article article);
 
+    /**
+     * Find article by id.
+     * @param id id
+     * @return article {@link Article}
+     * @throws ArticleNotFoundException article not found
+     */
     Article findArticleById(UUID id) throws ArticleNotFoundException;
 
+    /**
+     * Find article by header.
+     * @param header header.
+     * @return article {@link Article}
+     * @throws ArticleNotFoundException article not found
+     */
     Article findArticleByHeader(String header) throws ArticleNotFoundException;
 
+    /**
+     * Create new article with help of article dto.
+     * @param articleDto articleDto
+     * @return article {@link Article}
+     */
     Article createArticle(ArticleDto articleDto);
 }
